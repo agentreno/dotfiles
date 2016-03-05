2d80d639e029eaa68891bad3368cd5abf20f6da9
Karl <kturrell@hotmail.com>
2016-03-05 22:20:47 +0000

    Add installation script to update repos and install vim

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Get the current directory of this script wherever it was executed from
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Function to simplify running git outside of the repo directory
+function git-alt(){
+   local _dir="$1";
+   shift;
+   git --git-dir="${_dir}/.git" --work-tree="${_dir}" "$@"
+}
+
+# Function to highlight status updates from the script
+function report(){
+   tput bold;
+   echo "$@";
+   tput sgr0
+}
+
+# Update dotfiles itself and all of it's submodules
+report "Updating dotfiles and submodules"
+[ -d "$DOTFILES_DIR/.git" ] && git-alt $DOTFILES_DIR pull origin master
+pushd $DOTFILES_DIR > /dev/null
+git submodule update --init --recursive
+popd > /dev/null
+
+# Vim Setup
+# Rename any existing vimrc's or .vim/
+if [ -d ~/.vim ];
+then
+   report "Found existing ~/.vim folder, renaming to ~/.vimbak"
+   mv ~/.vim ~/.vimbak
+fi
+
+if [ -f ~/.vimrc ];
+then
+   report "Found existing ~/.vimrc file, renaming to ~/.vimrcbak"
+   mv ~/.vimrc ~/.vimrcbak
+fi
+
+# Link folder and config file
+report "Linking vim configuration files"
+ln -sfv "$DOTFILES_DIR/vimfiles/vimrc" ~/.vimrc
+ln -sfv "$DOTFILES_DIR/vimfiles" ~/.vim
