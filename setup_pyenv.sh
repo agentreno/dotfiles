33d648b15a881d01bb5934e6f260feed258a57e7
Karl <karl.hopkinson-turrell@nearform.com>
2018-08-17 19:09:36 +0000

    Run pyenv set up in a script file instead

---
+++ setup_pyenv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PATH="/home/karl/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv install 3.7.0
+pyenv install 2.7.15
+pyenv virtualenv 2.7.15 python2-tools
+pyenv virtualenv 3.7.0 python3-tools
+pyenv global 3.7.0 python3-tools python2-tools
+
+pyenv activate python2-tools
+pip install flake8
+pip install ipython
+pyenv deactivate
+
+pyenv activate python3-tools
+pip instal flake8
+pip install ipython
+pip install httpie
+pyenv deactivate
