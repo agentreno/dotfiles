e7c4f3eda7773490a84d08d424558715188cffa8
Karl <karl.hopkinson-turrell@nearform.com>
2018-08-18 09:02:05 +0100

    Replace legacy node installation with nvm and latest node

--- install.sh
+++ install.sh
@@ -74,11 +74,12 @@ sh -c $DOTFILES_DIR/setup_pyenv.sh
 mkdir ~/.config
 ln -sfv "$DOTFILES_DIR/.flake8" ~/.config/flake8
 
-# Install Node
-report "Installing Node 4.5.0"
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get -y install nodejs
-sudo ln -sfv /usr/bin/nodejs /usr/bin/node
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
 
 # Install xclip
 sudo apt-get -y install xclip
