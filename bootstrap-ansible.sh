cb0ab9c944d499dc7291dfdc4a430c02a885c93d
Karl <kturrell@hotmail.com>
2021-03-29 15:29:25 +0100

    fix ansible bootstrap

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y software-properties-common wget
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
-# TODO: Bootstrap ansible-playbook on the playbook.yml (possibly cloning repo first)
+wget https://github.com/agentreno/dotfiles/raw/master/playbook.yml
+
+if [ "$1" == "test" ]; then
+    ansible-playbook -e git_email=test@test.com -e user_password=test playbook.yml
+else
+    ansible-playbook playbook.yml
+fi
