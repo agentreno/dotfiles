07f85430c5c7a6c8abfc0be5d2645cd3eda5f4d2
Karl <karl@n8n.io>
2022-08-21 13:03:35 +0100

    chore: update pyenv python versions

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -6,11 +6,11 @@ export PATH="/home/$USER/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-pyenv install 3.9.2
-pyenv install 2.7.18
+pyenv install -s 3.10.6
+pyenv install -s 2.7.18
 pyenv virtualenv 2.7.18 python2-tools
-pyenv virtualenv 3.9.2 python3-tools
-pyenv global 3.9.2 python3-tools python2-tools
+pyenv virtualenv 3.10.6 python3-tools
+pyenv global 3.10.6 python3-tools python2-tools
 
 pyenv activate python2-tools
 pip install flake8
